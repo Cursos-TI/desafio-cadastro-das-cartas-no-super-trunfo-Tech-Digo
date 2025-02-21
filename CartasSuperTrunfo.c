e42b20e8095b7761503b4a2c0e0201e7de3d0a55
Tech-Digo <diegotech20@gmail.com>
2025-02-21 12:26:09 +0000

    supertrunfooo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,28 +1,111 @@
 #include <stdio.h>
+typedef struct supertunfo
+{
+           
+    char estado[3];
+    char codigoCidade[10];
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+    float densidade_populacional;
+    float pib_per_capita;
+} Cidade;
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
-           ("Brasil (A), EUA(B), SUIÇA(C), JAPÃO(D), MEXICO(E), CHINA(F),INDIA(G), ITALIA(H) ");
-    int populaçao = 
-    float area = 
-    double pontos turisticos =
-    char cod cicade = 
-    char nome[20] =
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int main (){
+        Cidade salvador = {"BA", "001", "Salvador",  2886698, 692.818, 63750.00, 30};
+        Cidade sao_paulo = {"SP", "002","São Paulo", 12300000, 1521.11, 714000.00, 50};
+        Cidade aracaju = {"SE", "003", "Aracaju", 602757, 181.9, 27364.00, 12 };
+        Cidade rio_de_janeiro = {"RJ", "004","Rio de Janeiro", 6211223, 1200.329, 359.63, 30};
+
+        salvador.densidade_populacional = 2886698/692.818;
+        salvador.pib_per_capita = 63750.00/2886698;
+
+        sao_paulo.densidade_populacional = 12300000/1521.11;
+        sao_paulo.pib_per_capita = 714000.00/12300000;
+        
+        aracaju.densidade_populacional = 602757/ 181.9;
+        aracaju.pib_per_capita = 27364.00/602757;
+
+        rio_de_janeiro.densidade_populacional = 6211223/1200.329;
+        rio_de_janeiro.pib_per_capita = 35963.00/6211223;
+
+        
+        printf("Dados da carta de Salvador: \n");
+        printf ("Estado: %s\n", salvador.estado);
+        printf("Código da carta: %s\n", salvador.codigoCidade);
+        printf("Nome: %s\n", salvador.nome);
+        printf("População: %d\n", salvador.populacao);
+        printf("Área: %.2f\n", salvador.area);
+        printf("PIB: %.2f\n", salvador.pib);
+        printf("Pontos Turísticos: %d\n", salvador.pontos_turisticos);
+        printf("PIB per capita: %.2f\n\n", salvador.pib_per_capita);
+
+         
+        printf("Dados da carta de São Paulo: \n");
+        printf ("Estado: %s\n", sao_paulo.estado);
+        printf("Código da carta: %s\n", sao_paulo.codigoCidade);
+        printf("Nome: %s\n", sao_paulo.nome);
+        printf("População: %d\n", sao_paulo.populacao);
+        printf("Área: %.2f\n", sao_paulo.area);
+        printf("PIB: %.2f\n", sao_paulo.pib);
+        printf("Pontos Turísticos: %d\n", sao_paulo.pontos_turisticos);
+        printf("PIB per capita: %.2f\n\n", sao_paulo.pib_per_capita);
+
+        printf("Dados da carta de Aracaju: \n");
+        printf ("Estado: %s\n", aracaju.estado);
+        printf("Código da carta: %s\n", aracaju.codigoCidade);
+        printf("Nome: %s\n", aracaju.nome);
+        printf("População: %d\n", aracaju.populacao);
+        printf("Área: %.2f\n", aracaju.area);
+        printf("PIB: %.2f\n", aracaju.pib);
+        printf("Pontos Turísticos: %d\n", aracaju.pontos_turisticos);
+        printf("PIB per capita: %.2f\n\n", aracaju.pib_per_capita);
+
+        printf("Dados da carta do Rio de janeiro: \n");
+        printf ("Estado: %s\n", rio_de_janeiro.estado);
+        printf("Código da carta: %s\n", rio_de_janeiro.codigoCidade);
+        printf("Nome: %s\n", rio_de_janeiro.nome);
+        printf("População: %d\n", rio_de_janeiro.populacao);
+        printf("Área: %.2f\n", rio_de_janeiro.area);
+        printf("PIB: %.2f\n", rio_de_janeiro.pib);
+        printf("Pontos Turísticos: %d\n", rio_de_janeiro.pontos_turisticos);
+        printf("PIB per capita: %.2f\n\n", rio_de_janeiro.pib_per_capita);
+
+
+if (salvador.pib_per_capita > aracaju.pib_per_capita){
+ printf("PIB per capita de salvador é maior \n !!");
+   } else {
+   printf(" Salvador tem o menor PIB comparado a Aracaju!!\n ");
+}
+
+
+if (rio_de_janeiro.pib_per_capita > sao_paulo.pib_per_capita ){
+ printf("PIB per capita do Rio de Janeiro é maior \n !!");
+   } else {
+   printf(" Rio de Janeiro tem o menor PIB comparado a São Paulo!!\n ");
+}
+
+
+    Cidade vencedora = salvador;
+
+    if (sao_paulo.populacao > vencedora.populacao) {
+        vencedora = sao_paulo;
+    }
+    if (aracaju.populacao > vencedora.populacao) {
+        vencedora = aracaju;
+    }
+    if (rio_de_janeiro.populacao > vencedora.populacao) {
+        vencedora = rio_de_janeiro;
+    }
+
+    printf("A carta vencedora com base na população é: \n");
+    printf("Nome: %s\n", vencedora.nome);
+    printf("População: %d\n", vencedora.populacao);
+
+
 
     return 0;
 }
